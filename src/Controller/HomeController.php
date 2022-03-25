bc62c7e1574732d226f283587433c5c3a9d157c2
thomasop <tdss33@hotmail.com>
2022-03-25 09:35:37 +0100

    update controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,27 +20,29 @@ class HomeController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $contactFormData = $form->getData();
-            $captcha = $_POST['g-recaptcha-response'];
-            $secret = '6LcZPf4eAAAAABtSPbNFcyzSaqYhEnEhk3Fb_gwX';
-            $url = "https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR'];
-            $response = file_get_contents($url);
-            $response = json_decode($response);
-            if ($response->success == true && $response->score >= 0.5) {
-                $message = (new Email())
-                        ->from(htmlspecialchars($contactFormData['email']))
-                        ->to('tdss33@hotmail.com')
-                        ->subject(htmlspecialchars($contactFormData['objet']))
-                        ->text('Expéditeur : '.htmlspecialchars($contactFormData['email']).\PHP_EOL.
-                        htmlspecialchars($contactFormData['message']),
-                            'text/plain');
-                    $mailer->send($message);
-                    $this->addFlash('success', 'Vous n\'êtes pas un robot, mail envoyé');
+            if (!empty($request->request->get('g-recaptcha-response')) && !empty($request->server->get('REMOTE_ADDR'))) {
+                $captcha = $request->request->get('g-recaptcha-response');
+                $remote = $request->server->get('REMOTE_ADDR');
+                $secret = 'secretkey';
+                $url = "https://www.google.com/recaptcha/api/siteverify?secret=" . $secret . "&response=" . $captcha . "&remoteip=" . $remote;
+                $response = file_get_contents($url);
+                $response = json_decode($response);
+                if ($response->success == true && $response->score >= 0.5) {
+                    $message = (new Email())
+                            ->from(htmlspecialchars($contactFormData['email']))
+                            ->to('mail@hotmail.com')
+                            ->subject(htmlspecialchars($contactFormData['objet']))
+                            ->text('Expéditeur : '.htmlspecialchars($contactFormData['email']).\PHP_EOL.
+                            htmlspecialchars($contactFormData['message']),
+                                'text/plain');
+                        $mailer->send($message);
+                        $this->addFlash('success', 'Vous n\'êtes pas un robot, mail envoyé');
+                        return $this->redirectToRoute('app_home');
+                } else {
+                    $this->addFlash('success', 'Vous êtes un robot, mail pas envoyé');
                     return $this->redirectToRoute('app_home');
-            } else {
-                $this->addFlash('success', 'Vous êtes un robot, mail pas envoyé');
-                return $this->redirectToRoute('app_home');
+                }
             }
-            
         }
         return $this->render('home/index.html.twig', [
             'form' => $form->createView()
